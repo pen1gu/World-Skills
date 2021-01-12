5adab69874a82a75b9218a85346401729cd0fc5c
HyunJun Yun <kkkths@naver.com>
2021-01-12 15:37:26 +0900

    update
    sql result 추가

--- Java/workspace/2020전국_전북/src/model/Connector.java
+++ Java/workspace/2020전국_전북/src/model/Connector.java
@@ -9,20 +9,26 @@ import java.sql.Statement;
 public class Connector {
 	static Connection connection;
 	static Statement statement;
-	
+
 	static {
 		try {
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/airdatabase_1?serverTimezone=UTC","user","1234");
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/airdatabase_1?serverTimezone=UTC", "user",
+					"1234");
 			statement = connection.createStatement();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
-	public ResultSet getSqlResult(String sql) {
+
+	public static ResultSet getSqlResult(String sql, Object... objects) {
 		PreparedStatement pst = null;
 		ResultSet rs = null;
 		try {
+			int cnt = 1;
+			for (Object object : objects) {
+				pst.setObject(cnt, object);
+				cnt++;
+			}
 			rs = pst.executeQuery(sql);
 		} catch (Exception e) {
 			e.printStackTrace();
