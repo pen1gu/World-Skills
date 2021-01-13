9e59b05793d074cebe9c1ea0e8b7353fee2fa056
HyunJun Yun <kkkths@naver.com>
2021-01-13 13:55:39 +0900

    update
    table

--- Java/workspace/2020전국_전북/src/frame/DestinationInfoPanel.java
+++ Java/workspace/2020전국_전북/src/frame/DestinationInfoPanel.java
@@ -3,6 +3,7 @@ package frame;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.sql.ResultSet;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -10,6 +11,8 @@ import javax.swing.JPanel;
 import javax.swing.JSpinner;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
+import model.Connector;
 
 public class DestinationInfoPanel extends JPanel {
 
@@ -21,8 +24,10 @@ public class DestinationInfoPanel extends JPanel {
 	JComboBox<String> cbStartRegion1 = new JComboBox<String>();
 	JComboBox<String> cbStartRegion2 = new JComboBox<String>();
 
-	DefaultTableModel model = new DefaultTableModel("Rend,Country,Departure,Arrive,Air,Type,StartTime,LastTime,Fare,선택".split(","),0);
-	JTable table = new JTable();
+	DefaultTableModel model = new DefaultTableModel(
+			"Rend,Country,Departure,Arrive,Air,Type,StartTime,LastTime,Fare,선택".split(","), 0);
+	JTable table = new JTable(model);
+
 	public DestinationInfoPanel() {
 		setSize(800, 600);
 
@@ -43,11 +48,26 @@ public class DestinationInfoPanel extends JPanel {
 		centerPanel.add(cbStartRegion1);
 		centerPanel.add(cbStartRegion2);
 
+		cbAirPlane.addActionListener(e -> changeTableList());
+		cbStartRegion1.addActionListener(e -> changeTableList());
+		cbStartRegion2.addActionListener(e -> changeTableList());
+
 		southPanel.add(new JLabel("도착지"), BorderLayout.NORTH);
-		southPanel.add(comp)
+		southPanel.add(table);
 
 		add(northPanel, BorderLayout.NORTH);
 		add(centerPanel, BorderLayout.CENTER);
 		add(southPanel, BorderLayout.SOUTH);
+	}
+
+	public void changeTableList() {
+		try (ResultSet rs = Connector.getSqlResult("select * from airinfo where ",
+				cbAirPlane.getSelectedItem(),
+				cbStartRegion1.getSelectedItem(),
+				cbStartRegion2.getSelectedItem())) {
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
