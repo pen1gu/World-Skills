a461463b8b703acda8f216490ab88a96f9fdf96a
HyunJun Yun <kkkths@naver.com>
2021-01-13 12:54:39 +0900

    update
    add table

--- Java/workspace/2020전국_전북/src/frame/DestinationInfoPanel.java
+++ Java/workspace/2020전국_전북/src/frame/DestinationInfoPanel.java
@@ -8,6 +8,8 @@ import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 public class DestinationInfoPanel extends JPanel {
 
@@ -19,9 +21,11 @@ public class DestinationInfoPanel extends JPanel {
 	JComboBox<String> cbStartRegion1 = new JComboBox<String>();
 	JComboBox<String> cbStartRegion2 = new JComboBox<String>();
 
+	DefaultTableModel model = new DefaultTableModel("Rend,Country,Departure,Arrive,Air,Type,StartTime,LastTime,Fare,선택".split(","),0);
+	JTable table = new JTable();
 	public DestinationInfoPanel() {
-		setSize(800,600);
-		
+		setSize(800, 600);
+
 		JPanel northPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 		JPanel centerPanel = new JPanel(new GridLayout(1, 0));
 		JPanel southPanel = new JPanel(new BorderLayout());
@@ -34,7 +38,13 @@ public class DestinationInfoPanel extends JPanel {
 		northPanel.add(spDay);
 		northPanel.add(new JLabel("일"));
 
-		centerPanel.add(new JLabel("출발자"));
+		centerPanel.add(new JLabel("출발지"));
+		centerPanel.add(cbAirPlane);
+		centerPanel.add(cbStartRegion1);
+		centerPanel.add(cbStartRegion2);
+
+		southPanel.add(new JLabel("도착지"), BorderLayout.NORTH);
+		southPanel.add(comp)
 
 		add(northPanel, BorderLayout.NORTH);
 		add(centerPanel, BorderLayout.CENTER);
