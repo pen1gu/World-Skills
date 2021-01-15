ab48b0a8d80475ca8a37018bf6da9e152055a577
hyunjun yun <kkkths@naver.com>
2021-01-15 09:35:36 +0900

    update
    have problem..

--- Java/workspace/2020전국_대전/src/model/Connector.java
+++ Java/workspace/2020전국_대전/src/model/Connector.java
@@ -5,10 +5,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-<<<<<<< Updated upstream
-=======
 import java.util.ArrayList;
->>>>>>> Stashed changes
 
 public class Connector {
 	static Connection connection;
@@ -24,16 +21,6 @@ public class Connector {
 		}
 	}
 
-<<<<<<< Updated upstream
-	public static ResultSet getSqlResult(String sql, Object... objects) {
-		ResultSet rs = null;
-
-		try (PreparedStatement pst = connection.prepareStatement(sql)) {
-			for (int i = 0; i < objects.length; i++)
-				pst.setObject(i + 1, objects[i]);
-
-			rs = pst.executeQuery();
-=======
 	public static ResultSet getSqlResult(String sql, Object... objects) { // 데이터 넣고 받아야할 때 ex) select
 		ResultSet rs = null;
 		try (PreparedStatement pst = connection.prepareStatement(sql)) {
@@ -42,8 +29,7 @@ public class Connector {
 			}
 
 			rs = pst.executeQuery();
-			
->>>>>>> Stashed changes
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -51,8 +37,18 @@ public class Connector {
 		return rs;
 	}
 
-<<<<<<< Updated upstream
-	public static void withoutSqlResult(String sql, Object... objects) {
+	public static ResultSet getSqlResultWithoutObject(String sql) {
+		ResultSet rs = null;
+
+		try {
+			rs = statement.executeQuery(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public static void getSqlWithoutResult(String sql, Object... objects) {
 		try (PreparedStatement pst = connection.prepareStatement(sql)) {
 			for (int i = 0; i < objects.length; i++)
 				pst.setObject(i + 1, objects[i]);
@@ -60,7 +56,9 @@ public class Connector {
 			pst.executeQuery();
 		} catch (Exception e) {
 			e.printStackTrace();
-=======
+		}
+	}
+
 	public static ArrayList<ArrayList<Object>> getSqlResults(String sql, int colCount, Object... objects) {
 		ArrayList<ArrayList<Object>> returnList = new ArrayList<ArrayList<Object>>();
 		ArrayList<Object> valueList;
@@ -93,7 +91,6 @@ public class Connector {
 		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i));
 			System.out.println();
->>>>>>> Stashed changes
 		}
 	}
 }
