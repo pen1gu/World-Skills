4b10e6e68b6a5015c4b6f831a7240ba5bc3fb57f
윤현준 <kkkths@naver.com>
2021-02-01 20:33:18 +0900

    create
    try factory method

---
+++ Java/workspace/2020전국_충남/src/model/AutoVehicle.java
@@ -0,0 +1,40 @@
+package model;
+
+import vehicle.AutoBicycle;
+import vehicle.Car;
+import vehicle.DisableCar;
+import vehicle.Truck;
+import vehicle.Van;
+
+public class AutoVehicle {
+
+	public VehicleFunction createFunction(String type, String path) {
+		VehicleFunction function = null;
+		switch (type) {
+		case "승용차": {
+			function = new Car(path);
+			break;
+		}
+		case "승합차": {
+			function = new Van(path);
+			break;
+		}
+		case "화물차": {
+			function = new Truck(path);
+			break;
+		}
+		case "장애인차": {
+			function = new DisableCar(path);
+			break;
+		}
+		case "오토바이차": {
+			function = new AutoBicycle(path);
+			break;
+		}
+		default:
+			throw new IllegalArgumentException("Unexpected value: " + type);
+		}
+
+		return function;
+	}
+}
