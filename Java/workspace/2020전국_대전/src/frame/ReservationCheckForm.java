5bfb71e71652e2732bceb383152931f490e20a40
윤현준 <kkkths@naver.com>
2021-02-01 20:29:59 +0900

    create
    form update

--- Java/workspace/2020전국_대전/src/frame/ReservationCheckForm.java
+++ Java/workspace/2020전국_대전/src/frame/ReservationCheckForm.java
@@ -1,17 +1,15 @@
 package frame;
 
-import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-
 public class ReservationCheckForm extends BaseFrame {
 
 	public ReservationCheckForm() {
-		super(600, 800, "예약");
+		super(600, 800, "예약확인");
+		for (String lbTxt : new String[] {"웨딩홀명","주소","수용인원","홀사용료","예식형태","식사종류","식사비용"}) {
 
+		}
 	}
 
+	public static void main(String[] args) {
+		new ReservationCheckForm().setVisible(true);
+	}
 }
