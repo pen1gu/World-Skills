2d26dcbbdcc3f54407fb2b6aba21d82523670e1d
윤현준 <kkkths@naver.com>
2021-02-02 01:31:24 +0900

    update
    insert turn method

--- Java/workspace/2020전국_충남/src/vehicle/Car.java
+++ Java/workspace/2020전국_충남/src/vehicle/Car.java
@@ -2,20 +2,41 @@ package vehicle;
 
 import static frame.BaseFrame.getImage;
 
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
 import model.VehicleFunction;
 
 public class Car extends VehicleFunction {
 
 	final String department = "A";
-	private String path;
+	int turnCheck = 0;
+//	boolean check = false;
 
 	private int x;
 	private int y;
 
 	public Car(String path) {
-		this.path = path;
-		setSize(60, 80);
-		setIcon(getImage(60, 80, path));
+		setSize(60, 60);
+		setIcon(getImage(60, 60, path));
+
+	}
+
+	@Override
+	protected void paintComponent(Graphics g) { // 공부
+		Graphics2D g2d = (Graphics2D) g;
+		if (turnCheck == 1) {
+			g2d.rotate(Math.PI / 2, 25, 30); // 오른쪽
+		} else if (turnCheck == -1) {
+			g2d.rotate(Math.PI * 1.5, 25, 30); // 왼쪽
+		} else if (turnCheck == 2) { // 위
+			g2d.rotate(Math.PI, 25, 30);
+		} else if (turnCheck == 0) { // 아래
+			g2d.rotate(Math.PI * 2, 25, 30);
+		}
+		super.paintComponent(g2d);
+		setPreferredSize(new Dimension(60, 60));
 	}
 
 	@Override
@@ -24,8 +45,8 @@ public class Car extends VehicleFunction {
 	}
 
 	@Override
-	public void turn(boolean leftOrRight) {
-		// TODO Auto-generated method stub
+	public void turn(int leftOrRight) {
+		turnCheck = leftOrRight;
 	}
 
 	public void setDepartmentLocation(int x, int y) {
