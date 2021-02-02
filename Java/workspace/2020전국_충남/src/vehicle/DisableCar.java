3026d579abf3233c374e875872f92a5f87cad98d
윤현준 <kkkths@naver.com>
2021-02-02 17:14:08 +0900

    delete
    unused

--- Java/workspace/2020전국_충남/src/vehicle/DisableCar.java
+++ Java/workspace/2020전국_충남/src/vehicle/DisableCar.java
@@ -1,27 +1,34 @@
-package vehicle;
-
-import java.awt.Color;
-
-import javax.swing.JLabel;
-
-import model.VehicleFunction;
-
-public class DisableCar extends VehicleFunction {
-	final String department = "D";
-
-	public DisableCar(String path) {
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public void move() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void turn(int leftOrRight) {
-		// TODO Auto-generated method stub
-
-	}
-}
+//package vehicle;
+//
+//import java.awt.Color;
+//
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//
+//import model.VehicleFunction;
+//
+//public class DisableCar extends VehicleFunction {
+//	final String department = "D";
+//
+//	public DisableCar(String path) {
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	@Override
+//	public void move() {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	public void turn(int leftOrRight) {
+//		// TODO Auto-generated method stub
+//
+//	}
+//
+//	@Override
+//	public void move(JPanel[][] department) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//}
--- Java/workspace/2020전국_충남/src/vehicle/Truck.java
+++ Java/workspace/2020전국_충남/src/vehicle/Truck.java
@@ -1,28 +1,35 @@
-package vehicle;
-
-import java.awt.Color;
-
-import javax.swing.JLabel;
-
-import model.VehicleFunction;
-
-public class Truck extends VehicleFunction {
-	
-	final String department = "C";
-
-	public Truck(String path) {
-		
-	}
-	
-	@Override
-	public void move() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void turn(int leftOrRight) {
-		// TODO Auto-generated method stub
-		
-	}
-}
+//package vehicle;
+//
+//import java.awt.Color;
+//
+//import javax.swing.JLabel;
+//import javax.swing.JPanel;
+//
+//import model.VehicleFunction;
+//
+//public class Truck extends VehicleFunction {
+//	
+//	final String department = "C";
+//
+//	public Truck(String path) {
+//		
+//	}
+//	
+//	@Override
+//	public void move() {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void turn(int leftOrRight) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void move(JPanel[][] department) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//}
