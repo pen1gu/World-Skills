c4838935b5573c7ba27dca4cde05cd57ff950b22
윤현준 <kkkths@naver.com>
2021-02-02 17:10:57 +0900

    update
    delete movement

--- Java/workspace/2020전국_충남/src/frame/MainForm.java
+++ Java/workspace/2020전국_충남/src/frame/MainForm.java
@@ -9,13 +9,16 @@ import java.awt.Graphics2D;
 import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.event.MouseAdapter;
+import java.util.Arrays;
 
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 
 import model.CurrentCarInfo;
+import model.ParkingAlgorithm;
 import model.VehicleFunction;
 
 public class MainForm extends BaseFrame {
@@ -25,13 +28,9 @@ public class MainForm extends BaseFrame {
 
 	CurrentCarInfo carInfo = new CurrentCarInfo();
 
-	JPanel[][] park = new JPanel[11][9];
-	boolean[][] isVisited = new boolean[11][9];
-	int[][] parked = new int[11][9];
+	JPanel[][] park = new JPanel[11][10];
 
-	// 들어가야할 주차 공간 미리 계산
-	// 그 공간을 기준으로 찾아감
-	// 영역은 상수로 지정
+	ParkingAlgorithm pa = new ParkingAlgorithm();
 
 	JPanel centerPanel;
 
@@ -42,7 +41,8 @@ public class MainForm extends BaseFrame {
 
 		Image backgroundImage = getImage(1000, 600, "./지급자료/image/주차장.jpg").getImage();
 
-		northPanel.add(createComponent(createButton("관리자", e -> openFrame(new AdminForm())), 150, 20, 100, 30));
+		northPanel.add(
+				createComponent(createButton("관리자", e -> openFrame(new AdminForm(), MainForm.this)), 150, 20, 100, 30));
 		northPanel.add(lbVehicle);
 
 		centerPanel = new JPanel(null) {
@@ -53,22 +53,37 @@ public class MainForm extends BaseFrame {
 			}
 		};
 
-		for (int i = 0; i < 9; i++)
-			for (int j = 0; j < 11; j++)
-				park[j][i] = new JPanel();
+		for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 11; j++) {
+				centerPanel.add(park[j][i] = createComponent(new JPanel(), 100, 36));
+				park[j][i].setBorder(new LineBorder(Color.blue));
+			}
 
 		for (int i = 0; i < 10; i++) { // parkA left
 			centerPanel.add(park[i + 1][0] = createComponent(new ParkArea(true, 50, 100 + (36 * i)), 50, 100 + (36 * i),
 					110, 36));
-			centerPanel.add(park[i + 1][2] = createComponent(new ParkArea(false, 270, 100 + (36 * i)), 270,
+			centerPanel.add(park[i + 1][3] = createComponent(new ParkArea(false, 270, 100 + (36 * i)), 270,
 					100 + (36 * i), 110, 36));
 
-			centerPanel.add(park[i + 1][3] = createComponent(new ParkArea(true, 390, 100 + (36 * i)), 390,
+			centerPanel.add(park[i + 1][4] = createComponent(new ParkArea(true, 390, 100 + (36 * i)), 390,
 					100 + (36 * i), 110, 36));
 
-			centerPanel.add(park[i + 1][5] = createComponent(new ParkArea(false, 670, 100 + (36 * i)), 670,
+			centerPanel.add(park[i + 1][6] = createComponent(new ParkArea(false, 670, 100 + (36 * i)), 670,
 					100 + (36 * i), 110, 36));
+
 		} // 이 부분을 현재 입차 상태인지 아닌지 구분하여 개발
+
+		for (int i = 0; i < 5; i++) {
+			centerPanel.add(park[i + 1][8] = createComponent(new ParkArea(false, 740, 100 + (40 * i)), 740,
+					103 + (40 * i), 110, 41));
+		}
+
+		for (int i = 0; i < 4; i++) {
+			centerPanel.add(park[i + 1][8] = createComponent(new ParkArea(false, 740, 340 + (25 * i)), 740,
+					340 + (25 * i), 110, 25));
+
+			// 오토바이랑 장애인 주차구역은 따로 구현 아 ㅋㅋ
+		}
 
 		// ui 구현 완료 -> 다익스트라까지만 입히기
 
@@ -106,66 +121,69 @@ public class MainForm extends BaseFrame {
 		vehicle.setLocation(890, 420);
 		centerPanel.add(vehicle);
 
-		moveThread = new Thread() {
-			@Override
-			public void run() {
-				super.run();
+		vehicle.move(park); // 구역도 보내야 함
 
-				int x = 890, y = 420;
-				int currentX = 0, currentY = 0;
-
-				try {
-					for (int i = 1; i <= 400; i++) {
-						vehicle.setLocation(x, currentY = y - i);
-						Thread.sleep(10);
-					}
-					y = currentY;
-
-					vehicle.turn(-1); // 자동차가 도는거를 구현해야한다
-
-					for (int i = 1; i <= 690; i++) {
-						vehicle.setLocation(currentX = x - i, y);
-						Thread.sleep(10);
-					}
-					x = currentX;
-
-					vehicle.turn(0);
-					
-					for (int i = 1; i <= 80; i++) {
-						vehicle.setLocation(x, currentY = y + i);
-						Thread.sleep(10);
-					}
-
-					y = currentY;
-
-					// if y 좌표가 자동차 길이 보다 작을 때
-
-					vehicle.turn(-1);
-					for (int i = 1; i <= 120; i++) {
-						vehicle.setLocation(currentX = x - i, y);
-						Thread.sleep(10);
-					}
-					currentX = x;
-
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		};
-
-		moveThread.start();
+//		moveThread = new Thread() {
+//			@Override
+//			public void run() {
+//				super.run();
+//
+//				int x = 890, y = 420;
+//				int currentX = 0, currentY = 0;
+//
+//				try {
+//					for (int i = 1; i <= 400; i++) {
+//						vehicle.setLocation(x, currentY = y - i);
+//						Thread.sleep(10);
+//					}
+//					y = currentY;
+//
+//					vehicle.turn(-1); // 자동차가 도는거를 구현해야한다
+//
+//					for (int i = 1; i <= 690; i++) {
+//						vehicle.setLocation(currentX = x - i, y);
+//						Thread.sleep(10);
+//					}
+//					x = currentX;
+//
+//					vehicle.turn(0);
+//
+//					for (int i = 1; i <= 80; i++) {
+//						vehicle.setLocation(x, currentY = y + i);
+//						Thread.sleep(10);
+//					}
+//
+//					y = currentY;
+//
+//					// if y 좌표가 자동차 길이 보다 작을 때
+//
+//					vehicle.turn(-1);
+//					for (int i = 1; i <= 120; i++) {
+//						vehicle.setLocation(currentX = x - i, y);
+//						Thread.sleep(10);
+//					}
+//					currentX = x;
+//
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		};
+//
+//		moveThread.start();
 	}
 
 	public static void main(String[] args) {
 		new MainForm().setVisible(true);
 	}
 
-	class ParkWhetherDialog extends JDialog {
+	class ParkWhetherDialog extends JFrame {
 		public ParkWhetherDialog() {
 			setSize(400, 150);
 			setLocationRelativeTo(null);
 			setLayout(new GridLayout(1, 0));
-			add(createButton("입차", e -> changeFrame(new SelectParkVehicleForm(MainForm.this, carInfo))));
+			add(createButton("입차",
+					e -> openFrame(new SelectParkVehicleForm(MainForm.this, carInfo), ParkWhetherDialog.this)));
 			add(createButton("취소", e -> dispose()));
 		}
 	}
