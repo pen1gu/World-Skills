ef35e6914557997b6090ab3f4d0b21617850a8d0
윤현준 <kkkths@naver.com>
2021-02-03 23:37:53 +0900

    create baseframe

---
+++ Java/workspace/2020전국_충북/src/frame/BaseFrame.java
@@ -0,0 +1,96 @@
+package frame;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Insets;
+import java.awt.Toolkit;
+import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.WindowConstants;
+
+public class BaseFrame extends JFrame {
+
+	static Connection connection;
+	static Statement statement;
+
+	static {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/computer?serverTimezone=UTC", "user",
+					"1234");
+			statement = connection.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public BaseFrame(int width, int height, String title) {
+		setSize(width, height);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setTitle(title);
+	}
+
+	public static void errorMessage(String message) {
+		JOptionPane.showMessageDialog(null, message, "", JOptionPane.ERROR_MESSAGE);
+	}
+
+	public static void informationMessage(String message) {
+		JOptionPane.showMessageDialog(null, message, "", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public static JLabel createLabel(JLabel label, Font font) {
+		label.setFont(font);
+		return label;
+	}
+
+	public static <T extends JComponent> T createComponent(T comp, int x, int y, int width, int height) {
+		comp.setBounds(x, y, width, height);
+		return comp;
+	}
+
+	public static <T extends JComponent> T createComponent(T comp, int width, int height) {
+		comp.setPreferredSize(new Dimension(width, height));
+		return comp;
+	}
+
+	public static JButton createButton(String txt, ActionListener actionListener) {
+		JButton btn = new JButton(txt);
+		btn.addActionListener(actionListener);
+		return btn;
+	}
+
+	public static JButton createButtonWithColor(String txt, ActionListener actionListener, Color color) {
+		JButton btn = new JButton(txt);
+		btn.addActionListener(actionListener);
+		btn.setBackground(color);
+		return btn;
+	}
+
+	public static JButton createButtonWithoutMargin(String txt, ActionListener actionListener) {
+		JButton btn = new JButton(txt);
+		btn.addActionListener(actionListener);
+		btn.setMargin(new Insets(0, 0, 0, 0));
+		return btn;
+	}
+
+	public static ImageIcon getImage(String path, int width, int height) {
+		return new ImageIcon(
+				Toolkit.getDefaultToolkit().getImage(path).getScaledInstance(width, height, Image.SCALE_SMOOTH));
+	}
+
+	public void openFrame(JFrame frame) {
+		dispose();
+		frame.setVisible(true);
+	}
+}
