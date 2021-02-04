a5bfbbdfaa229c74c7d58a36a10dba5961a54549
hyunjun yun <kkkths@naver.com>
2021-02-04 16:58:41 +0900

    create
    connection

---
+++ Java/workspace/2020전국_충북/src/model/ConnectionManager.java
@@ -0,0 +1,76 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ConnectionManager {
+	Connection connection = null;
+	Statement statement = null;
+	PreparedStatement pst = null;
+
+	public void connect() {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost/computer?serverTimezone=UTC", "user",
+					"1234");
+			statement = connection.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void close() {
+		try {
+			if (pst != null)
+				pst.close();
+			if (statement != null)
+				statement.close();
+			if (connection != null)
+				connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet select(String sql) {
+		ResultSet rs = null;
+		try {
+			rs = statement.executeQuery(sql);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
+	public void update(String sql) {
+		try {
+			statement.executeUpdate(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void execute(String sql) {
+		try {
+			statement.execute(sql);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public ResultSet getSqlResults(String sql, Object... objects) {
+		ResultSet rs = null;
+		try (PreparedStatement pst = connection.prepareStatement(sql)) {
+			for (int i = 0; i < objects.length; i++) {
+				pst.setObject(i + 1, objects[i]);
+			}
+			rs = pst.executeQuery();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return rs;
+	}
+}
