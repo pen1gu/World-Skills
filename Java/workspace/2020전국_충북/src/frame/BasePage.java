4d8bfc1053154431cc02c851c28cdcedabc66519
hyunjun yun <kkkths@naver.com>
2021-02-05 16:41:29 +0900

    create
    rebuild change page

--- Java/workspace/2020전국_충북/src/frame/BasePage.java
+++ Java/workspace/2020전국_충북/src/frame/BasePage.java
@@ -1,14 +1,31 @@
 package frame;
 
+import java.awt.Dimension;
+
 import javax.swing.JPanel;
 
 public class BasePage extends JPanel {
 	public BasePage(int width, int height) {
-		setSize(width, height);
+		setPreferredSize(new Dimension(width, height));
 	}
 
-	public void changePage(JPanel closePanel, JPanel openPanel) {
-		closePanel.setVisible(false);
+	public void changePage(JPanel openPanel) {
+		MainFrame.contentsPanel.removeAll();
+
+		MainFrame.contentsPanel.add(openPanel);
 		openPanel.setVisible(true);
+
+	}
+
+	public void closePage() {
+		MainFrame.contentsPanel.removeAll();
+		MainFrame.contentsPanel.add(MainFrame.mainPage);
+		MainFrame.contentsPanel.revalidate();
+		MainFrame.contentsPanel.repaint();
+	}
+
+	public void reconfiguration() {
+		revalidate();
+		repaint();
 	}
 }
