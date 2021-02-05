41642b1e077e4874fd8c1c7cf77ab4733e2db9c6
hyunjun yun <kkkths@naver.com>
2021-02-05 16:42:21 +0900

    create
    user data

---
+++ Java/workspace/2020전국_충북/src/model/UserData.java
@@ -0,0 +1,22 @@
+package model;
+
+public class UserData {
+	private int userNo;
+	private String userName;
+
+	public int getUserNo() {
+		return userNo;
+	}
+
+	public void setUserNo(int userNo) {
+		this.userNo = userNo;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+}
