b05ad0049e8ec9d38ad1aca1514d4578120dc31e
hyunjun yun <kkkths@naver.com>
2021-02-08 11:51:19 +0900

    create
    buy product

---
+++ Java/workspace/2020전국_충북/src/frame/ProductBuyPage.java
@@ -0,0 +1,7 @@
+package frame;
+
+public class ProductBuyPage extends BasePage {
+	public ProductBuyPage(String name, String description, String sellerName, int price, int rating, int count) {
+		super(1, 1);
+	}
+}
