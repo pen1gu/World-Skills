f64a48705768d42dc542577623ddf69ef3dc9c48
hyunjun yun <kkkths@naver.com>
2021-02-19 14:52:04 +0900

    create
    유저 정보 폼

---
+++ Java/workspace/2016지방_20210219/src/model/CustomerInfo.java
@@ -0,0 +1,58 @@
+package model;
+
+public class CustomerInfo {
+	private String code;
+	private String name;
+	private String birth;
+	private String telNumber;
+	private String address;
+	private String company;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+
+	public String getTelNumber() {
+		return telNumber;
+	}
+
+	public void setTelNumber(String telNumber) {
+		this.telNumber = telNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+}
