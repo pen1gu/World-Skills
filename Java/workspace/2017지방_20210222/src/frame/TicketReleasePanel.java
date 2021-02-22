bb3fe41c8d78d30eaf563b34d31352b5623127af
hyunjun yun <kkkths@naver.com>
2021-02-23 08:49:17 +0900

    create
    티켓

---
+++ Java/workspace/2017지방_20210222/src/frame/TicketReleasePanel.java
@@ -0,0 +1,11 @@
+package frame;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class TicketReleasePanel extends JPanel {
+	public TicketReleasePanel() {
+		setPreferredSize(new Dimension(800, 520));
+	}
+}
