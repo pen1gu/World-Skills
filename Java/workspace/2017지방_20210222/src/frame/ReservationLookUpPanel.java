925c54cb10eb8de29ac44814dc150ed875fd4fe5
hyunjun yun <kkkths@naver.com>
2021-02-23 08:49:03 +0900

    create
    예약

---
+++ Java/workspace/2017지방_20210222/src/frame/ReservationLookUpPanel.java
@@ -0,0 +1,11 @@
+package frame;
+
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+public class ReservationLookUpPanel extends JPanel {
+	public ReservationLookUpPanel() {
+		setPreferredSize(new Dimension(800, 520));
+	}
+}
