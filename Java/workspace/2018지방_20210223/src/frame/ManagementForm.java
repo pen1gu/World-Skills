a464b99c5d9d89af860c3988a6cd46603cd083d1
윤현준 <kkkths@naver.com>
2021-02-28 13:09:12 +0900

    update
    manage

--- Java/workspace/2018지방_20210223/src/frame/ManagementForm.java
+++ Java/workspace/2018지방_20210223/src/frame/ManagementForm.java
@@ -1,17 +1,27 @@
 package frame;
 
+import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class ManagementForm extends BaseFrame{
+public class ManagementForm extends BaseFrame {
 	public ManagementForm() {
-		super(500, 300, "");
-		JPanel northPanel = new JPanel(new FlowLayout());
-		northPanel.add(createButton("메뉴등록", e->openFrame(new MenuAddForm())));
-		northPanel.add(createButton("메뉴관리", e));
-		northPanel.add(createButton("결제조회", e));
-		northPanel.add(createButton("메뉴별주분현황", e));
-		northPanel.add(createButton("종료", e->openFrame(new MainForm())));
+		super(500, 350, "관리");
+		JPanel northPanel = createComponent(new JPanel(new FlowLayout()), 500, 35);
+		JLabel centerImage = new JLabel(getImage(500, 250, "./지급자료/main.jpg"));
+
+		northPanel.add(createButton("메뉴등록", e -> openFrame(new MenuAddForm("신규 메뉴 등록"))));
+		northPanel.add(createButton("메뉴관리", e -> openFrame(new MenuMagamentForm())));
+		northPanel.add(createButton("결제조회", e -> openFrame(new PayFormLookUpForm())));
+		northPanel.add(createButton("메뉴별주분현황", e -> openFrame(new PayListForm())));
+		northPanel.add(createButton("종료", e -> openFrame(new MainForm())));
+
+		northPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+
+		add(northPanel, BorderLayout.NORTH);
+		add(centerImage);
 	}
 }
