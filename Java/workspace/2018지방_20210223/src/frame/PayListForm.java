15d5b05aa3bf4f2332264673b9757961bc7a36e4
윤현준 <kkkths@naver.com>
2021-02-28 16:31:04 +0900

    update
    최종 완성

--- Java/workspace/2018지방_20210223/src/frame/PayListForm.java
+++ Java/workspace/2018지방_20210223/src/frame/PayListForm.java
@@ -3,22 +3,24 @@ package frame;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableRowSorter;
 
 public class PayListForm extends BaseFrame {
 
 	JLabel lbAmount = new JLabel();
 
-	DefaultTableModel model = new DefaultTableModel(new String[] { "종류,주문수량" }, 0);
+	DefaultTableModel model = new DefaultTableModel(new String[] { "종류", "주문수량" }, 0);
 	JTable table = new JTable(model);
 
 	public PayListForm() {
-		super(300, 190, "메뉴별 주문현황");
+		super(300, 200, "메뉴별 주문현황");
 
 		JPanel northPanel = createComponent(new JPanel(new FlowLayout(FlowLayout.RIGHT)), 300, 35);
 		JPanel centerPanel = new JPanel(new BorderLayout());
@@ -27,24 +29,38 @@ public class PayListForm extends BaseFrame {
 		northPanel.add(createButton("닫기", e -> openFrame(new ManagementForm())));
 
 		southPanel.add(lbAmount);
-		JScrollPane pane = createComponent(new JScrollPane(table), 300, 120);
+		JScrollPane pane = createComponent(new JScrollPane(table), 300, 80);
 
-		lbAmount.setText("");
+		try (PreparedStatement pst = connection.prepareStatement(
+				"select c.cuisineName, count(*)  from orderlist as odl inner join cuisine as c on odl.cuisineNo = c.cuisineNo group by c.cuisineName;")) {
+			ResultSet rs = pst.executeQuery();
 
-		try (PreparedStatement pst = connection.prepareStatement("select * from")){
-			
+			while (rs.next()) {
+				model.addRow(new Object[] { rs.getObject(1), rs.getObject(2) });
+			}
+
+			for (int i = 0; i < 2; i++) {
+				table.getColumnModel().getColumn(i).setCellRenderer(centerRender);
+			}
+
+			int amount = 0;
+			for (int i = 0; i < 4; i++) {
+				System.out.println(model.getValueAt(i, 1));
+				amount += Integer.parseInt(model.getValueAt(i, 1).toString());
+
+			}
+
+			lbAmount.setText(String.format("합계: %d개", amount));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
+		table.setRowSorter(new TableRowSorter(model));
+
 		centerPanel.add(pane);
 
 		add(northPanel, BorderLayout.NORTH);
 		add(centerPanel);
 		add(southPanel, BorderLayout.SOUTH);
-	}
-
-	public void changeAmount() {
-		
 	}
 }
